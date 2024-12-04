9378e82b533e682b115ede75f2c6e342d5d8b87d
dinar <saetonskie@mail.ru>
2024-12-04 14:35:49 +0300

    config changes, checkin pages added, api added

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,6 @@
 import style from './App.module.scss';
+import Signin from '../pages/authorization/Signin';
+import Signup from '../pages/authorization/Signup';
 
 function App() {
 	return <div className={style.App}></div>;
